3cdb281eca43720b3f545d59499a3b685196dbc8
amr elkhalifa <aee29@njit.edu>
2025-05-02 15:39:32 -0400

    updated project to SDK 53 and added logo

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,3 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-
-module.exports = {
-  ...config,
-  resolver: {
-    ...config.resolver,
-    sourceExts: [...config.resolver.sourceExts, 'jsx', 'js', 'ts', 'tsx'],
-  },
-};
+module.exports = getDefaultConfig(__dirname);
